81443a57c08be9061cd27b07aa4efe0d6eb83c16
s99113266 <41054903+s99113266@users.noreply.github.com>
2019-11-19 23:55:16 +0800

    作業3

---
+++ UnityProject/Assets/Scripts/FoxScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class FoxScript : MonoBehaviour
+{
+    [Header("移動速度"),Range(0.1f, 100f)]
+    public float Speed = 10.0f;
+    public Rigidbody2D rb2D;
+    public SpriteRenderer _spriteRenderer;
+    
+    private void Start()
+    {
+        //rb2D.gameObject.AddComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        switch (h)
+        {
+            case 1:
+                print("1");
+                rb2D.AddForce(new Vector2(1, 0) * Speed);
+                _spriteRenderer.flipX = false;
+                break;
+            case -1:
+                print("-1");
+                rb2D.AddForce(new Vector2(1, 0) * -Speed);
+                _spriteRenderer.flipX = true;
+                break;
+        }
+        //print( * Speed);
+    }
+}
